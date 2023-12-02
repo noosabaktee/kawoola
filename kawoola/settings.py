342dff850d044693bfdf7f450062394b7a4b3016
noosabaktee <ramanusabakti03@gmail.com>
2023-12-02 20:38:36 +0700

    migrate to postgres

--- kawoola/settings.py
+++ kawoola/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'kawoola.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kawoola',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -143,6 +154,5 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = 'static/img/'
 
 # SITE_URL = 'http://127.0.0.1:8000/' # this will reduce the Sites framework db call.
-SITE_URL = 'https://kawoola.vercel.app/' # this will reduce the Sites framework db call.
+SITE_URL = 'https://kawoola.up.railway.app/' # this will reduce the Sites framework db call.
 
-WSGI_APPLICATION = 'vercel_app.wsgi.app'
