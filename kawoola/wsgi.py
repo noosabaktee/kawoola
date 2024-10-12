d5e2b92dab8a01eb725546d9bee4831faf3b8dcc
noosabaktee <ramanusabakti03@gmail.com>
2024-10-12 20:03:17 +0700

    bismillah update wsgi bisa deploy static

--- kawoola/wsgi.py
+++ kawoola/wsgi.py
@@ -15,6 +15,6 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kawoola.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), '\static'))
 
 app = application
